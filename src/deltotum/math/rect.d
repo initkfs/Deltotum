dea10a25ccaf9c35c8647961219e5059097edafb
initkfs <(none)>
2022-08-10 22:33:46 +0300

    Add ellipse and circle, update api to work with geometry

--- src/deltotum/math/rect.d
+++ src/deltotum/math/rect.d
@@ -1,6 +1,7 @@
 module deltotum.math.rect;
 
 import deltotum.math.vector2d : Vector2D;
+import deltotum.math.circle2d : Circle2D;
 
 /**
  * Authors: initkfs
@@ -12,16 +13,40 @@ struct Rect
     double width = 0;
     double height = 0;
 
+    bool contains(double x, double y) const @nogc nothrow pure @safe
+    {
+        return x >= this.x && y >= this.y && x < right && y < bottom;
+    }
+
+    bool contains(Vector2D point) const @nogc nothrow pure @safe
+    {
+        return contains(point.x, point.y);
+    }
+
+    bool contains(Circle2D circle) const @nogc nothrow pure @safe
+    {
+        return (circle.x + circle.radius <= right) && (circle.x - circle.radius >= x) && (
+            circle.y + circle.radius <= bottom) && (
+            circle.y - circle.radius >= y);
+    }
+
+    bool contains(Rect rect) const @nogc nothrow pure @safe
+    {
+        float minX = rect.x;
+        float maxX = right;
+
+        float minY = rect.y;
+        float maxY = bottom;
+
+        return ((minX > x && minX < right) && (maxX > x && maxX < right))
+            && ((minY > y && minY < bottom) && (maxY > y && maxY < bottom));
+    }
+
     bool overlaps(Rect other) const @nogc nothrow pure @safe
     {
         auto isOverlaps = (other.right > x) && (other.x < right) && (other.bottom > y) && (
             other.y < bottom);
         return isOverlaps;
-    }
-
-    bool contains(double x, double y) const @nogc nothrow pure @safe
-    {
-        return x >= this.x && y >= this.y && x < right && y < bottom;
     }
 
     double right() const @nogc nothrow pure @safe
@@ -64,6 +89,30 @@ struct Rect
     {
         Vector2D maxYPos = {right, bottom};
         return maxYPos;
+    }
+
+    Vector2D center() const @nogc nothrow pure @safe
+    {
+        return Vector2D(x + halfWidth, y + halfHeight);
+    }
+
+    double aspectRatio() const @nogc nothrow pure @safe
+    {
+        import std.math.operations : isClose;
+
+        if (isClose(height, 0))
+        {
+            return 0;
+        }
+
+        return width / height;
+    }
+
+    string toString() const
+    {
+        import std.format : format;
+
+        return format("x: %s, y: %s, width: %s, height: %s", x, y, width, height);
     }
 
 }
