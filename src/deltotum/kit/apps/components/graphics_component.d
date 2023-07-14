7699dedad93503faf56059a3f1e28ad262ba592d
initkfs <user@user>
2023-07-14 14:29:25 +0300

    Minor improvements. Remove finalization from graphics component

--- src/deltotum/kit/apps/components/graphics_component.d
+++ src/deltotum/kit/apps/components/graphics_component.d
@@ -30,69 +30,66 @@ class GraphicsComponent : UniComponent
         @Service Capability _cap;
     }
 
-    //bypass hijacking
-    override void build(UniComponent uniComponent)
+    void build(GraphicsComponent gComponent)
     {
-        if (auto graphicComponent = cast(GraphicsComponent) uniComponent)
-        {
-            buildFromParent!GraphicsComponent(graphicComponent, this);
-            return;
-        }
-        buildFromParent(uniComponent, this);
+        buildFromParent(gComponent, this);
     }
 
-    final bool hasAsset() @nogc nothrow pure @safe
+    override void build(UniComponent uniComponent)
+    {
+        super.build(uniComponent);
+    }
+
+    bool hasAsset() @nogc nothrow pure @safe
     {
         return _asset !is null;
     }
 
-    final Asset asset() @nogc nothrow pure @safe
+    Asset asset() @nogc nothrow pure @safe
     out (_asset; _asset !is null)
     {
         return _asset;
     }
 
-    final void asset(Asset assetManager) pure @safe
+    void asset(Asset assetManager) pure @safe
     {
         import std.exception : enforce;
 
         enforce(assetManager !is null, "Asset manager must not be null");
         _asset = assetManager;
-
     }
 
-    final bool hasWindow() @nogc nothrow pure @safe
+    bool hasWindow() @nogc nothrow pure @safe
     {
         return _window !is null;
     }
 
-    final Window window() @nogc nothrow pure @safe
+    Window window() @nogc nothrow pure @safe
     out (_window; _window !is null)
     {
         return _window;
     }
 
-    final void window(Window window) pure @safe
+    void window(Window window) pure @safe
     {
         import std.exception : enforce;
 
         enforce(window !is null, "Window must not be null");
         _window = window;
-
     }
 
-    final bool hasInput() @nogc nothrow pure @safe
+    bool hasInput() @nogc nothrow pure @safe
     {
         return _input !is null;
     }
 
-    final Input input() @nogc nothrow pure @safe
+    Input input() @nogc nothrow pure @safe
     out (_input; _input !is null)
     {
         return _input;
     }
 
-    final void input(Input input) pure @safe
+    void input(Input input) pure @safe
     {
         import std.exception : enforce;
 
@@ -100,18 +97,18 @@ class GraphicsComponent : UniComponent
         _input = input;
     }
 
-    final bool hasAudio() @nogc nothrow pure @safe
+    bool hasAudio() @nogc nothrow pure @safe
     {
         return _audio !is null;
     }
 
-    final Audio audio() @nogc nothrow pure @safe
+    Audio audio() @nogc nothrow pure @safe
     out (_audio; _audio !is null)
     {
         return _audio;
     }
 
-    final void audio(Audio audio) pure @safe
+    void audio(Audio audio) pure @safe
     {
         import std.exception : enforce;
 
@@ -119,18 +116,18 @@ class GraphicsComponent : UniComponent
         _audio = audio;
     }
 
-    final bool hasGraphics() @nogc nothrow pure @safe
+    bool hasGraphics() @nogc nothrow pure @safe
     {
         return _graphics !is null;
     }
 
-    final Graphics graphics() @nogc nothrow pure @safe
+    Graphics graphics() @nogc nothrow pure @safe
     out (_graphics; _graphics !is null)
     {
         return _graphics;
     }
 
-    final void graphics(Graphics graphics) pure @safe
+    void graphics(Graphics graphics) pure @safe
     {
         import std.exception : enforce;
 
@@ -138,18 +135,18 @@ class GraphicsComponent : UniComponent
         _graphics = graphics;
     }
 
-    final bool hasScreen() @nogc nothrow pure @safe
+    bool hasScreen() @nogc nothrow pure @safe
     {
         return _screen !is null;
     }
 
-    final Screen screen() @nogc nothrow pure @safe
+    Screen screen() @nogc nothrow pure @safe
     out (_screen; _screen !is null)
     {
         return _screen;
     }
 
-    final void screen(Screen screen) pure @safe
+    void screen(Screen screen) pure @safe
     {
         import std.exception : enforce;
 
@@ -157,18 +154,18 @@ class GraphicsComponent : UniComponent
         _screen = screen;
     }
 
-    final bool hasExtension() @nogc nothrow pure @safe
+    bool hasExtension() @nogc nothrow pure @safe
     {
         return _ext !is null;
     }
 
-    final Extension ext() @nogc nothrow pure @safe
+    Extension ext() @nogc nothrow pure @safe
     out (_ext; _ext !is null)
     {
         return _ext;
     }
 
-    final void ext(Extension ext) pure @safe
+    void ext(Extension ext) pure @safe
     {
         import std.exception : enforce;
 
@@ -176,18 +173,18 @@ class GraphicsComponent : UniComponent
         _ext = ext;
     }
 
-    final bool hasCap() @nogc nothrow pure @safe
+    bool hasCap() @nogc nothrow pure @safe
     {
         return _cap !is null;
     }
 
-    final Capability cap() @nogc nothrow pure @safe
+    Capability cap() @nogc nothrow pure @safe
     out (_cap; _cap !is null)
     {
         return _cap;
     }
 
-    final void cap(Capability caps) pure @safe
+    void cap(Capability caps) pure @safe
     {
         import std.exception : enforce;
 
