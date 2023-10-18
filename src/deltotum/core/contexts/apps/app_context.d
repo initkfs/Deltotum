f81e4f9b02ecf04955fcaa5959aab6044e527af4
initkfs <user@user>
2023-10-18 23:02:49 +0300

    Improvements in core context

--- src/deltotum/core/contexts/apps/app_context.d
+++ src/deltotum/core/contexts/apps/app_context.d
@@ -7,74 +7,37 @@ import std.typecons : Nullable;
  */
 class AppContext
 {
-    const
-    {
-        bool isDebug;
-        bool isSilent;
-    }
+    bool isDebug;
+    bool isSilent;
 
-    private const
+    private
     {
-        string _workingDir;
+        string _workDir;
         string _dataDir;
         string _userDir;
     }
 
-    this(const string workingDir = null, const string dataDir = null, const string userDir = null, const bool isDebug = true, const bool isSilent = false)
+    this(string workDir = null, string dataDir = null, string userDir = null, bool isDebug = true, bool isSilent = false) pure @safe
     {
-        import std.string : strip;
-        import std.exception : enforce;
-        import std.file : isDir, exists;
-
-        if (workingDir !is null)
-        {
-            enforce(workingDir.strip.length > 0, "Working directory must not be empty");
-            if (!workingDir.exists || !workingDir.isDir)
-            {
-                import std.file : FileException;
-
-                throw new FileException(
-                    "Working directory does not exist or is not a directory: " ~
-                        workingDir);
-            }
-
-            _workingDir = workingDir;
-        }
-
-        if (dataDir !is null)
-        {
-            enforce(dataDir.strip.length > 0, "Data directory must not be empty");
-            if (!dataDir.exists || !dataDir.isDir)
-            {
-                import std.file : FileException;
-
-                throw new FileException(
-                    "Data directory does not exist or is not a directory: " ~ dataDir);
-            }
-
-            _dataDir = dataDir;
-        }
-
-        if (userDir !is null)
-        {
-            enforce(userDir.strip.length > 0, "User directory must not be empty");
-            if (!userDir.exists || !userDir.isDir)
-            {
-                import std.file : FileException;
-
-                throw new FileException(
-                    "User directory does not exist or is not a directory: " ~ userDir);
-            }
-            _userDir = userDir;
-        }
-
+        _workDir = workDir;
+        _dataDir = dataDir;
+        _userDir = userDir;
         this.isDebug = isDebug;
         this.isSilent = isSilent;
     }
 
-    Nullable!string workingDir() const @nogc nothrow pure @safe
+    this(string workDir = null, string dataDir = null, string userDir = null, bool isDebug = true, bool isSilent = false) immutable pure @safe
     {
-        return Nullable!string(_workingDir);
+        _workDir = workDir;
+        _dataDir = dataDir;
+        _userDir = userDir;
+        this.isDebug = isDebug;
+        this.isSilent = isSilent;
+    }
+
+    Nullable!string workDir() const @nogc nothrow pure @safe
+    {
+        return Nullable!string(_workDir);
     }
 
     Nullable!string dataDir() const @nogc nothrow pure @safe
--- src/deltotum/core/contexts/context.d
+++ src/deltotum/core/contexts/context.d
@@ -1,14 +1,27 @@
 module deltotum.core.contexts.context;
 
-import deltotum.core.contexts.apps.app_context: AppContext;
+import deltotum.core.contexts.apps.app_context : AppContext;
+
 /**
  * Authors: initkfs
  */
 class Context
 {
-   const AppContext appContext;
+    const AppContext appContext;
 
-    this(const AppContext appContext){
+    this(const AppContext appContext) pure @safe
+    {
         this.appContext = appContext;
     }
+
+    this(immutable AppContext appContext) immutable pure @safe
+    {
+        this.appContext = appContext;
+    }
+}
+
+unittest
+{
+    immutable c = new immutable Context(new AppContext);
+    assert(is(typeof(c.appContext) : immutable(AppContext)));
 }
