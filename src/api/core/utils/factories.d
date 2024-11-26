7d51be56a9737e185715f58cf7a54ea499180a2a
initkfs <user@user>
2024-11-26 10:36:10 +0300

    Remove tostring and factory kit due to complexity

--- src/api/core/utils/factories.d
+++ src/api/core/utils/factories.d
@@ -3,72 +3,7 @@ module api.core.utils.factories;
 /**
  * Authors: initkfs
  */
-
-class FactoryKit(Entity, Type = string)
-{
-    private
-    {
-        Entity delegate()[Type] factories;
-    }
-
-    this(scope void delegate(
-            scope void delegate(Type, Entity delegate()) @safe
-    ) @safe factoriesProvider) @safe
-    {
-        factoriesProvider((type, Entity delegate() entityFactory) {
-            factories[type] = entityFactory;
-        });
-    }
-
-    Entity create(Type type)
-    {
-        return factory(type)();
-    }
-
-    Entity delegate() factory(Type type)
-    {
-        if (!has(type))
-        {
-            import std.conv : to;
-
-            throw new Exception("Not found factory for type " ~ type.to!string);
-        }
-        return factories[type];
-    }
-
-    bool has(Type type) const pure @safe
-    {
-        return (type in factories) !is null;
-    }
-
-}
-
-unittest
-{
-
-    enum Ftype
-    {
-        a,
-        b,
-    }
-
-    class A
-    {
-    }
-
-    class B
-    {
-    }
-
-    auto fk = new FactoryKit!(Object, Ftype)((builder) {
-        builder(Ftype.a, () => new A);
-        builder(Ftype.b, () => new B);
-    });
-
-    assert(cast(A)(fk.create(Ftype.a)));
-    assert(cast(B)(fk.create(Ftype.b)));
-}
-
+ 
 struct ProviderFactory(T)
 {
     T delegate() getNew;
