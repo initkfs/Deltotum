72e247f118e1ee7ad7aab919ba4bf2a16e331cb7
initkfs <user@user>
2024-11-30 13:07:49 +0300

    Add indeterminate state to checks

--- src/api/dm/gui/controls/switches/checks/check_switch.d
+++ src/api/dm/gui/controls/switches/checks/check_switch.d
@@ -12,11 +12,17 @@ class CheckSwitch : BaseBiswitch
     protected
     {
         Sprite marker;
+        Sprite markerContainer;
+        Sprite indeterminate;
     }
 
     bool isCreateMarker;
     Sprite delegate(Sprite) onMarkerCreate;
     void delegate(Sprite) onMarkerCreated;
+
+    bool isCreateIndeterminate = true;
+    Sprite delegate(Sprite) onIndeterminateCreate;
+    void delegate(Sprite) onIndeterminateCreated;
 
     double markerWidth = 0;
     double markerHeight = 0;
@@ -62,55 +68,91 @@ class CheckSwitch : BaseBiswitch
         }
     }
 
-    Sprite newMarker()
+    Sprite newMarkerContainer()
     {
         import api.dm.gui.containers.stack_box : StackBox;
 
+        auto markerContainer = new StackBox;
+        markerContainer.resize(markerWidth, markerHeight);
+        markerContainer.isBorder = true;
+        return markerContainer;
+    }
+
+    Sprite newMarker()
+    {
         import api.dm.kit.sprites.shapes.convex_polygon : ConvexPolygon;
         import api.dm.kit.graphics.styles.graphic_style : GraphicStyle;
-        import api.dm.gui.containers.stack_box : StackBox;
 
         assert(markerWidth > 0);
         assert(markerHeight > 0);
 
         auto marker = new ConvexPolygon(markerWidth / 2, markerHeight / 2, GraphicStyle(1, theme.colorAccent, true, theme
                 .colorAccent), 3);
+        return marker;
+    }
 
-        auto markerContainer = new class StackBox
-        {
-            this()
-            {
-                super(markerWidth, markerHeight);
-            }
+    Sprite newIndeterminateMarker()
+    {
+        import api.dm.kit.sprites.shapes.rectangle : Rectangle;
+        import api.dm.kit.graphics.styles.graphic_style : GraphicStyle;
 
-            override void isVisible(bool value)
-            {
-                marker.isVisible = value;
-            }
-        };
+        assert(markerWidth > 0);
+        assert(markerHeight > 0);
 
-        markerContainer.isBorder = true;
-        buildInitCreate(markerContainer);
-
-        markerContainer.addCreate(marker);
-        return markerContainer;
+        auto marker = new Rectangle(markerWidth / 2, 5, GraphicStyle(1, theme.colorAccent, true, theme
+                .colorAccent));
+        return marker;
     }
 
     override void create()
     {
         super.create;
 
+        markerContainer = newMarkerContainer;
+        addCreate(markerContainer);
+
         if (isCreateMarker)
         {
             auto newMarker = newMarker();
             marker = onMarkerCreate ? onMarkerCreate(newMarker) : newMarker;
-            addCreate(marker);
+            if (markerContainer)
+            {
+                markerContainer.addCreate(marker);
+            }
+            else
+            {
+                addCreate(marker);
+            }
             if (onMarkerCreated)
             {
                 onMarkerCreated(marker);
             }
+        }
 
-            markerState(isOn);
+        if (isCreateIndeterminate)
+        {
+            auto newMarker = newIndeterminateMarker();
+            indeterminate = onIndeterminateCreate ? onIndeterminateCreate(newMarker) : newMarker;
+            if (markerContainer)
+            {
+                markerContainer.addCreate(indeterminate);
+            }
+            else
+            {
+                addCreate(indeterminate);
+            }
+            if (onIndeterminateCreated)
+            {
+                onIndeterminateCreated(indeterminate);
+            }
+        }
+
+        //isIndeterminate?
+        //markerState(isOn);
+        if (indeterminate)
+        {
+            markerState = false;
+            isIndeterminate = true;
         }
 
         if (isCreateMarkerListeners)
@@ -118,6 +160,19 @@ class CheckSwitch : BaseBiswitch
             assert(marker);
             onPointerUp ~= (ref e) { toggle; };
         }
+    }
+
+    bool isIndeterminate() => indeterminate && indeterminate.isVisible;
+
+    bool isIndeterminate(bool value)
+    {
+        if (!indeterminate)
+        {
+            return false;
+        }
+
+        indeterminate.isVisible = value;
+        return true;
     }
 
     protected bool markerState(bool value)
@@ -136,8 +191,12 @@ class CheckSwitch : BaseBiswitch
     override bool isOn(bool value, bool isRunListeners = true)
     {
         bool isSetState = super.isOn(value, isRunListeners);
-        if (isSetState && marker)
+        if (isSetState)
         {
+            if(isIndeterminate){
+                isIndeterminate = false;
+            }
+
             markerState = _state;
         }
 
