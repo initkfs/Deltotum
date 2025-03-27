10ff4d05dfb74edb359ac9aa233711458f56da80
initkfs <user@user>
2025-03-27 21:01:41 +0300

    Improvements in signals synthesis

--- src/api/dm/kit/media/synthesis/signal_synthesis.d
+++ src/api/dm/kit/media/synthesis/signal_synthesis.d
@@ -62,9 +62,123 @@ void whiteNoise(T)(Random rnd, T[] buffer, double sampleRateHz, double amplitude
     });
 }
 
+void chaoticLogistic(T)(T[] buffer, double r, double sampleRateHz, double amplitude0to1 = 1.0, size_t channels = 2)
+{
+    double x = 0.5;
+    onBuffer(buffer, sampleRateHz, amplitude0to1, channels, (i, time) {
+        x = r * x * (1.0 - x);
+        return (x - 0.5f) * 2.0;
+    });
+}
+
+void poissonNoise(T)(Random rnd, T[] buffer, double rate, double sampleRateHz, double amplitude0to1 = 1.0, size_t channels = 2)
+{
+    double threshold = 1.0 - rate / sampleRateHz;
+
+    onBuffer(buffer, sampleRateHz, amplitude0to1, channels, (i, time) {
+        return (rnd.between0to1 > threshold) ? 1 : 0;
+    });
+}
+
+void fractalNoise(T)(Random rnd, T[] buffer, double sampleRateHz, double amplitude0to1 = 1.0, size_t channels = 2)
+{
+    import core.stdc.stdlib : malloc, free;
+
+    void* tempPtr = malloc(buffer.length * double.sizeof);
+    assert(tempPtr);
+
+    scope (exit)
+    {
+        free(tempPtr);
+    }
+
+    double[] temp = (cast(double*) tempPtr)[0 .. buffer.length];
+    assert(buffer.length == temp.length);
+
+    double value = 0;
+
+    foreach (ref t; temp)
+    {
+        value += (rnd.between0to1 - 0.5) * 0.1;
+        value *= 0.99;
+        t = value;
+    }
+
+    double max = 0.0;
+    foreach (ref t; temp)
+    {
+        const abst = Math.abs(t);
+        if (abst > max)
+        {
+            max = abst;
+        }
+    }
+
+    onBuffer(buffer, sampleRateHz, amplitude0to1, channels, (i, time) {
+        return temp[i] / max;
+    });
+}
+
+void phaseDistortion(T)(T[] buffer, double freqHz, double distortion, double sampleRateHz, double amplitude0to1 = 1.0, size_t channels = 2)
+{
+    import std.math : fmod;
+
+    double phaseDt = freqHz / sampleRateHz;
+    double phase = 0;
+
+    onBuffer(buffer, sampleRateHz, amplitude0to1, channels, (i, time) {
+        phase = fmod(phase + phaseDt, 1.0);
+        double distortPhase = phase < distortion ? phase / distortion : (
+            1.0 - phase) / (1.0 - distortion);
+        return Math.sin(2.0f * Math.PI * distortPhase);
+    });
+}
+
+void polynomialWaveshaper(T)(T[] buffer, double freqHz, double a, double b, double c, double sampleRateHz, double amplitude0to1 = 1.0, size_t channels = 2)
+{
+    double phaseDt = freqHz / sampleRateHz;
+    double phase = 0;
+
+    onBuffer(buffer, sampleRateHz, amplitude0to1, channels, (i, time) {
+        phase = fmod(phase + phaseDt, 1.0);
+        dobule x = 2.0 * phase - 1.0;
+        double y = a * x + b * x * x * x + c * x * x * x * x * x;
+        return y;
+    });
+}
+
+void diracImpulses(T)(T[] buffer, size_t period, double sampleRateHz, double amplitude0to1 = 1.0, size_t channels = 2)
+{
+    onBuffer(buffer, sampleRateHz, amplitude0to1, channels, (i, time) {
+        return (i % period == 0) ? 1 : 0;
+    });
+}
+
 void fm(T)(T[] buffer, double fc, double fm, double index, double sampleRateHz, double amplitude0to1 = 1.0, size_t channels = 2)
 {
     onBuffer(buffer, sampleRateHz, amplitude0to1, channels, (i, time) {
         return Math.sin(2 * Math.PI * fc * time + index * Math.sin(2 * PI * fm * time));
+    });
+}
+
+void am(T)(T[] buffer, double fc, double fm, double sampleRateHz, double amplitude0to1 = 1.0, size_t channels = 2)
+{
+    double carrierDt = fc / sampleRateHz;
+    double modDt = fm / sampleRateHz;
+
+    double carrierPhase = 0;
+    double modPhase = 0;
+
+    import std.math : fmod;
+
+    onBuffer(buffer, sampleRateHz, amplitude0to1, channels, (i, time) {
+
+        carrierPhase = fmod(carrierPhase + carrierDt, 1.0);
+        modPhase = fmod(modPhase + modDt, 1.0);
+
+        double mod = 0.5 * (1.0 + Math.sin(Math.PI2 * modPhase));
+        double carrier = Math.sin(Math.PI2 * carrierPhase);
+
+        return mod * carrier;
     });
 }
