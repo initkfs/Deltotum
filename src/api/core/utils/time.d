4e6c93ea811bd14acc3c3f87065ce4acdaebccf5
initkfs <user@user>
2026-02-21 20:13:52 +0300

    Minor improvements in utils.time

--- src/api/core/utils/time.d
+++ src/api/core/utils/time.d
@@ -3,17 +3,25 @@ module api.core.utils.time;
 /**
  * Authors: initkfs
  */
-import core.stdc.time : time_t, time, gmtime, tm, strftime;
+import core.stdc.time : time_t, time, gmtime, localtime, tm, strftime;
 import std.string : toStringz;
 
-tm* utcTime()
+enum defaultTimeFormat = "%Y-%m-%d %H:%M:%S";
+
+time_t nowTime() => time(null);
+
+tm* utcTime(time_t time) => gmtimeut(&time);
+tm* utcTime() => utcTime(nowTime);
+
+tm* localTime(time_t time) => localtime(&time);
+tm* localTime() => localTime(nowTime);
+
+size_t formatTime(char[] buffer, tm* time, string format = defaultTimeFormat)
 {
-    time_t now = time(null);
-    tm* utc = gmtime(&now);
-    return utc;
+    return strftime(buffer.ptr, buffer.length, format.toStringz, time);
 }
 
-size_t utcTimeBuff(char[] buffer, string format = "%Y-%m-%d %H:%M:%S")
+size_t utcTimeBuff(char[] buffer, string format = defaultTimeFormat)
 {
-    return strftime(buffer.ptr, buffer.length, format.toStringz, utcTime);
+    return formatTime(buffer, utcTime, format);
 }
